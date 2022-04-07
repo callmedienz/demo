93e2575d9e1962f7b11c527120a0095219007b44
callmedienz <huydngch190066@fpt.edu.vn>
2022-04-08 00:01:53 +0700

    demo

---
+++ src/Controller/MobileController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Mobile;
+use App\Form\MobileType;
+use App\Repository\MobileRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MobileController extends AbstractController
+{
+      // SELECT * FROM Mobile (ORDER BY id ASC) : oldest => newest
+      #[Route('/', name: 'mobile_index')]
+      public function viewMobileList (ManagerRegistry $doctrine) {
+          $mobiles = $doctrine->getRepository(Mobile::class)->findAll();
+          return $this->render('mobile/index.html.twig',
+                                 [
+                                     'mobiles' => $mobiles
+                                 ]);
+      }
+      // SELECT * FROM Mobile ORDER BY id DESC : newest => oldest
+      #[Route('/sort', name: 'mobile_desc')]
+      public function sortMobileByIdDesc (MobileRepository $mobileRepository) {
+         $mobiles = $mobileRepository->sortId();
+         return $this->render('mobile/index.html.twig',
+                                 [
+                                     'mobiles' => $mobiles
+                                 ]);
+      }
+ 
+      //DELETE FROM Mobile WHERE id = '$id'
+      #[Route('/delete/{id}', name: 'mobile_delete')]
+      public function deleteMobile (ManagerRegistry $doctrine, $id) {
+         $mobile = $doctrine->getRepository(Mobile::class)->find($id);
+         $manager = $doctrine->getManager();
+         $manager->remove($mobile);
+         $manager->flush();
+         return $this->redirectToRoute('mobile_desc');
+      }
+ 
+      #[Route('/asc', name: 'quantity_asc')]
+      public function sortQuantityAsc (MobileRepository $mobileRepository) {
+         $mobiles = $mobileRepository->sortQuantityAsc();
+         return $this->render('mobile/index.html.twig',
+                                 [
+                                     'mobiles' => $mobiles
+                                 ]);
+      }
+ 
+      #[Route('/desc', name: 'quantity_desc')]
+      public function sortQuantityDesc (MobileRepository $mobileRepository) {
+         $mobiles = $mobileRepository->sortQuantityDesc();
+         return $this->render('mobile/index.html.twig',
+                                 [
+                                     'mobiles' => $mobiles
+                                 ]);
+      }
+ 
+      #[Route('/search', name: 'mobile_search')]
+     public function searchMobile (MobileRepository $mobileRepository, Request $request) {
+         $name = $request->get('keyword');
+         $mobiles = $mobileRepository->searchByName($name);
+         //nếu kết quả tìm kiếm rỗng thì đẩy thông báo lỗi về view
+         if ($mobiles == null) {
+            $this->addFlash('Error','Mobile not found !');
+         }
+         return $this->render('mobile/index.html.twig',
+                                [
+                                    'mobiles' => $mobiles
+                                ]);
+     }
+
+     #[Route('/add', name: 'mobile_add')]
+     public function addMobile (Request $request, ManagerRegistry $doctrine) {
+         $mobile = new Mobile;
+         $form = $this->createForm(MobileType::class,$mobile);
+         $form->handleRequest($request);
+         if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $doctrine->getManager();
+            $manager->persist($mobile);
+            $manager->flush();
+            $this->addFlash("Success","Add new mobile succeed !");
+            return $this->redirectToRoute('mobile_desc');
+         }
+         return $this->renderForm('mobile/add.html.twig',
+                                [
+                                    'mobileForm' => $form
+                                ]);
+     }
+}
---
+++ src/Form/MobileType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Mobile;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MobileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,
+            [
+                'label' => 'Mobile Name',
+                'required' => true,
+                'attr' => [
+                    'minlength' => 3,
+                    'maxlength' => 30
+                ]
+            ])
+            ->add('price', MoneyType::class,
+            [
+                'label' => 'Mobile Price',
+                'currency' => 'USD'
+            ])
+            ->add('quantity', IntegerType::class,
+            [
+                'label' => 'Mobile Quantity',
+                'attr' => [
+                    'min' => 5,
+                    'max' => 50
+                ]
+            ])
+            ->add('image', TextType::class,
+            [
+                'label' => 'Mobile Image'
+            ])
+            ->add('Save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Mobile::class,
+        ]);
+    }
+}
